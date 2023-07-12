f81849baea40ee7fac0af8c35e5b0ab1bf43218a
YuRobert <111642519+YuRobert@users.noreply.github.com>
2023-07-12 13:14:20 +0800

    ss
    ss

---
+++ security/src/main/java/com/waterspringer/security/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.waterspringer.security.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Robert17
+ * @create2023-07-12-13:08
+ */
+
+@RestController
+public class LoginController
+{
+
+}
